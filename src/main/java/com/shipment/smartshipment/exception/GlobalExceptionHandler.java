41d61a2a4f53a5c786cbc11b0c81d32fa724f8a0
Yash Jangra <urmilajangra303@gmail.com>
2026-08-22 00:45:10 +0530

    feat: add global exception handling for shipments

--- src/main/java/com/shipment/smartshipment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shipment/smartshipment/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.shipment.smartshipment.exception;
 
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,8 +12,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(ShipmentNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleShipmentNotFound(ShipmentNotFoundException ex){
-        return ex.getMessage() ;
+    public ErrorResponse handleShipmentNotFound(
+            ShipmentNotFoundException ex,
+            HttpServletRequest request) {
+
+        return new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
     }
 
 }
